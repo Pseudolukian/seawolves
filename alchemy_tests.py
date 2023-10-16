ae77da5a16c12dc4b5de487ec0aaad5b6a08d7e0
Pseudolukian <pynanist@gmail.com>
2023-10-16 06:03:35 +0000

    Add new model

--- alchemy_tests.py
+++ alchemy_tests.py
@@ -1,16 +1,18 @@
-from sqlalchemy import select, update
+from sqlalchemy import select, update, insert
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from typing import Generator, List, Dict, Union, Tuple
 from models.alchemy_models import Seauser
-from models.pydantic_models import UserRegestrationModel, AcceptedUserRegistration
+from models.pydantic_models import UserRegestrationModel, AcceptedUserRegistration, AcceptedUserDeleted, UserUpdateModel, UserUpdateStatusModel
 import asyncio
 from contextlib import asynccontextmanager
 import json
 from fastapi import HTTPException
+
 #===========JSON Data load==========================
 admins = dict(json.load(open("test_data.json", "r")))["admins"]
 simple_users = dict(json.load(open("test_data.json", "r")))["simple users"]
+
 #=============Working with DB========================
 engine = create_async_engine("postgresql+asyncpg://seawolve:wolve@localhost/seawolves",
     future=True, echo=False, execution_options={"isolation_level": "AUTOCOMMIT"})
@@ -37,34 +39,80 @@ class UserDAL:
         mail = await self.db_session.execute(select(Seauser.email).where(Seauser.email == email))
         return mail.first()
 
+    async def delete(self, user_id:str):
+        action = await self.db_session.execute(update(Seauser)
+                                       .where(Seauser.id == user_id)
+                                       .values(status = "deleted")
+                                       .returning(Seauser.nick_name, Seauser.status,Seauser.email))
+        return action.first()
+
     async def create(self,
                             nick_name: Seauser.nick_name,
                             email: Seauser.email,
                             hashed_password: Seauser.hashed_password) -> Seauser:
         email_check = await self.check_email(email=email)
         if email_check is None:
-            new_user = Seauser(
-                               nick_name=nick_name,
-                               email=email,
+            new_user = Seauser(nick_name=nick_name,email=email,
                                hashed_password=hashed_password)
-            self.db_session.add(new_user)
-            await self.db_session.flush()
-            return new_user
+            action = await self.db_session.execute(insert(Seauser).values(nick_name = new_user.nick_name,
+                                                                          email = new_user.email,
+                                                                          hashed_password = new_user.hashed_password)
+                                                                    .returning(Seauser.nick_name, Seauser.email))
+
+            return action.first()
         else:
             raise HTTPException(status_code=400, detail="Email is already in use.")
 
-    async def update(self, user_id:str, **kwargs):
-        update_date = await self.db_session.execute(update(Seauser)
-                            .where(Seauser.id == user_id)
-                            .values(kwargs))
-        return update_date
+    async def update(self, user_id_to_update: Seauser.id = None,
+                     nick_name_to_update: Seauser.nick_name = None,
+                     first_name_to_update: Seauser.first_name = None,
+                     last_name_to_update: Seauser.last_name = None,
+                     email_to_update: Seauser.email = None,
+                     hashed_pass_to_update: Seauser.hashed_password = None,
+                     age_to_update: Seauser.age = None,
+                     exp_to_update: Seauser.experience = None,
+                     about_to_update: Seauser.about = None,
+                     head_line_to_update: Seauser.head_line = None):
 
-    async def delete(self, user_id:str):
-        action = await self.db_session.execute(update(Seauser)
-                                       .where(Seauser.id == user_id)
-                                       .values(status = "deleted")
-                                       .returning(Seauser.nick_name))
-        return action
+        values_to_update = {
+            "nick_name": nick_name_to_update,
+            "first_name": first_name_to_update,
+            "last_name": last_name_to_update,
+            "email": email_to_update,
+            "hashed_password": hashed_pass_to_update,
+            "age": age_to_update,
+            "experience": exp_to_update,
+            "about": about_to_update,
+            "head_line": head_line_to_update
+        }
+
+        actual_updates = {key: value for key, value in values_to_update.items() if value is not None}
+
+        if not actual_updates:
+            raise ValueError("No values provided for update.")
+
+        action = await self.db_session.execute(
+            update(Seauser)
+            .where(Seauser.id == user_id_to_update)
+            .values(**actual_updates)
+            .returning(*[getattr(Seauser, k) for k in actual_updates.keys()])
+        )
+
+        return actual_updates
+
+    async def user_change_status(self, user_id:Seauser.id, new_status:Seauser.status):
+        actual_status = await self.db_session.execute(select(Seauser.status).where(Seauser.id == user_id))
+        new_status = UserUpdateStatusModel(status=new_status)
+
+        if actual_status.first()[0] == new_status.status.value:
+            raise HTTPException(status_code=400, detail="New and old statuses are the same.")
+        else:
+            update_status = await self.db_session.execute(update(Seauser)
+                                                          .where(Seauser.id == user_id)
+                                                          .values(status = new_status.status.value)
+                                                          .returning(Seauser.status))
+
+        return update_status.first()[0]
 
 #===============================================
 
@@ -87,22 +135,30 @@ async def test_create_user(nick_name: str,
         out = output_data.model_validate(action)
         print(out)
 
-async def test_update_user_data(l_n:str):
+
+async def test_update_user_data(user_id_to_update:str, age:Seauser.age, nick_nam:Seauser.nick_name):
     async with get_db() as session:
         user = UserDAL(db_session=session)
-        result = await user.update(mail="pynanist@gmail.com", change_last_name= l_n)
+        result = await user.update(user_id_to_update=user_id_to_update, age_to_update=age, nick_name_to_update=nick_nam)
         print(result)
 
-"""
-async def test_delete_user(user_id:str, output_model:Accept_User_Delete = Accept_User_Delete):
+
+async def test_delete_user(user_id_to_delete:str, output_model: AcceptedUserDeleted = AcceptedUserDeleted):
 
     async with get_db() as session:
         user = UserDAL(db_session=session)
-        action = await user.delete(user_id=user_id)
-        output_data = output_model(nick_name = action.first()[0])
-        print(output_data)
-"""
+        action = await user.delete(user_id=user_id_to_delete)
+        print(action)
+
+async def test_user_change_status(user_id_to_change:str, new_user_status:str):
+    async with get_db() as session:
+        user = UserDAL(db_session=session)
+        action = await user.user_change_status(user_id=user_id_to_change, new_status=new_user_status)
+        print(action)
+
 
 if __name__ == "__main__":
-    asyncio.run(test_create_user(email="katkov@gmail.com", hashed_password="12345678",
-                                 nick_name="pseudolukain"))
+    asyncio.run(test_user_change_status(user_id_to_change="51ce233e-3e40-42c7-91da-284ce45003d2", new_user_status="registrated"))
+    #asyncio.run(test_update_user_data(user_id_to_update="51ce233e-3e40-42c7-91da-284ce45003d2", age=20, nick_nam="Pseudo"))
+    #asyncio.run(test_create_user(nick_name="some_nick5", email="some@mail5.com", hashed_password="324324234"))
+    #asyncio.run(test_delete_user(user_id_to_delete="5dc6279f-1f27-4d95-b502-124b0c55b70c"))
--- models/pydantic_models.py
+++ models/pydantic_models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field, EmailStr
 from datetime import datetime
 from typing import Optional
+from enum import  Enum
 
 #=======Input data models===========#
 class UserExperience(BaseModel):
@@ -9,13 +10,22 @@ class UserExperience(BaseModel):
     position: str = ""
     responsibilities: str = ""
 
-class UserBaseInputModel(BaseModel):
-    email: EmailStr
-    hashed_password: str = "12345678"
+class UserChangeStatus(Enum):
+    registrated: str = "registrated"
+    banned: str = "banned"
+    deleted: str = "deleted"
+    stopped: str = "stopped"
+
+class UserUpdateStatusModel(BaseModel):
+    status:UserChangeStatus
+
+class UserUpdateModel(BaseModel):
+    nick_name: str = Field(default="def_nick_name", pattern="^[a-zA-Z0-9_]*$", max_length=30)
     first_name: str = Field(default="First name", max_length=30, pattern="^[a-zA-Z]*$")
     last_name: str = Field(default="Last name", max_length=50, pattern="^[a-zA-Z]*$")
+    email: EmailStr
+    hashed_password: str = "12345678"
     age: int = Field(default=18, gt=17, lt=81, description="Age must be between 18 and 80")
-    nick_name: str = Field(default="def_nick_name", pattern="^[a-zA-Z0-9_]*$", max_length=30)
     experience: UserExperience = UserExperience()
     about: str = ""
     head_line: str = Field(default="Last name", max_length=150)
@@ -35,3 +45,7 @@ class AcceptedUserRegistration(TunedModel):
     nick_name: str = None
     email: str = None
 
+class AcceptedUserDeleted(TunedModel):
+    nick_name: str = None
+    email: str = None
+    status:str = None
